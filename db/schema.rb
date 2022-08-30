4499a6a83a6f0693687c39013f0f7e12f46dde24
StuartAhmet <sma0190@gmail.com>
2022-08-30 16:13:59 +0100

    added two fields to create item (phone and location) - also updated the items database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_135031) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_150351) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_135031) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "location"
+    t.integer "phone"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
