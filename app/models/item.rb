71ec91c7f58a963090a45445a45af75d0847cfab
Eduard Gobjila <edikgobjila01@gmail.com>
2022-08-30 11:32:20 +0100

    added a create, new, method to the controlller and views, but the form creating is not saving need to work more on it

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :users
+  CATEGORIES = ['Pets', 'Vehicles', 'Bycicles', 'Clothes', 'Audio & Stereo', 'Jobs', 'Electronics', 'Health and Beauty', 'Others']
+
+  has_many :purchases
 end
