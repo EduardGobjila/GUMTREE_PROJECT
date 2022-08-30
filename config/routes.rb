9affaef9b8339aa9c5cacf22aa73000d3f56f567
Rory Leslie Melville <rlesliemelville1@gmail.com>
2022-08-30 15:00:32 +0100

    photo

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  get 'items/index'
-  get 'items/show'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
